87b6b097526acfda67c1d7d7f8bb5430eb9150f7
Andrew Tredennick <atredenn@gmail.com>
2016-10-20 08:49:46 -0600

    adds bison SSM forecast model

--- code/bison_forecast.R
+++ code/bison_forecast.R
@@ -21,6 +21,8 @@ library(plyr)
 library(rjags)
 library(coda)
 library(devtools)
+library(rnoaa)
+library(ncdf4)
 # install_github("atredennick/ecoforecastR", force=TRUE) # get latest version
 library(ecoforecastR)
 
@@ -29,22 +31,31 @@ library(ecoforecastR)
 ####
 ####  Load data, aggregate to yearly values
 ####
-sage_raw <- read.csv("../data/ARTR_quadratCover.csv")
-sage_raw$year <- sage_raw$year+1900 # makes a calendar year
-sage_dat <- ddply(sage_raw, .(year), summarise,
-                  tot_cover = round(mean(totCover)/100)) # converts to percent cover, integer
+bison_raw <- read.csv("../data/YNP_bison_population_size.csv", row.names = 1)
+bison_dat <- bison_raw[,2:ncol(bison_raw)]
+bison_dat$count.mean <- round(bison_dat$count.mean)
 
-clim_dat <- read.csv("../data/idaho_climate.csv")
+
+##  Retrieve climate data from NOAA
+# lat <- 44.4280
+# lon <- 110.5885
+# out <- ncdc(datasetid='GHCND', stationid='GHCND:USW00014895', datatypeid='PRCP', startdate = '2010-05-01', enddate = '2010-10-31', limit=500)
+# 
+# ##  Get forecasted snow
+# gefs_variables()
+# snow_depth = gefs("Snow_depth_surface_ens",lat,lon,raw=TRUE)
+
+# clim_dat <- read.csv("../data/idaho_climate.csv")
 
 ## Merge observation and climate data
-sage_climate_dat <- merge(sage_dat, clim_dat)
+# bison_climate_dat <- merge(bison_dat, clim_dat)
 
 
 ####
 ####  Fit forecasting GLM
 ####
-my_model <- list(obs="tot_cover", fixed="~ppt1+TmeanSpr1", random=NULL, n.iter=5000)
-fitted_model <- fit_dlm_pois(model=my_model, data=sage_climate_dat)
+my_model <- list(obs="count.mean", fixed=NULL, random=NULL, n.iter=5000)
+fitted_model <- fit_dlm_lnorm(model=my_model, data=bison_dat)
 predictions <- rbind(fitted_model$predict[[1]],
                      fitted_model$predict[[2]],
                      fitted_model$predict[[3]])
@@ -52,11 +63,11 @@ median_predictions <- apply(predictions, MARGIN = 2, FUN = "median")
 upper_predictions <- apply(predictions, MARGIN = 2, FUN = function(x){quantile(x, probs = 0.975)})
 lower_predictions <- apply(predictions, MARGIN = 2, FUN = function(x){quantile(x, probs = 0.025)})
 
-prediction_df <- data.frame(year = sage_climate_dat$year,
-                            observation = sage_climate_dat$tot_cover,
-                            median_prediction = median_predictions,
-                            upper_prediction = upper_predictions,
-                            lower_prediction = lower_predictions)
+prediction_df <- data.frame(year = bison_dat$year,
+                            observation = bison_dat$count.mean,
+                            median_prediction = exp(median_predictions),
+                            upper_prediction = exp(upper_predictions),
+                            lower_prediction = exp(lower_predictions))
 
 
 
@@ -68,10 +79,10 @@ ggplot(prediction_df, aes(x=year))+
   geom_ribbon(aes(ymax=upper_prediction, ymin=lower_prediction), fill=pred_color, color=NA, alpha=0.2)+
   geom_line(aes(y=median_prediction), color=pred_color)+
   geom_point(aes(y=observation))+
-  ylab("Cover of sagebrush (%)")+
+  ylab("Number of bison")+
   xlab("Year")+
   theme_few()
-ggsave(filename = "../figures/sage_calibration.png", width = 4, height = 3, units = "in", dpi=120)
+ggsave(filename = "../figures/bison_calibration.png", width = 4, height = 3, units = "in", dpi=120)
 
 
 
