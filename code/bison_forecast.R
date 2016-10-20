e908a65f289dad3282583acc14c0d9f2fedcc7ab
Andrew Tredennick <atredenn@gmail.com>
2016-10-20 12:07:30 -0600

    adds climate to bison

--- code/bison_forecast.R
+++ code/bison_forecast.R
@@ -35,26 +35,27 @@ bison_raw <- read.csv("../data/YNP_bison_population_size.csv", row.names = 1)
 bison_dat <- bison_raw[,2:ncol(bison_raw)]
 
 
-##  Retrieve climate data from NOAA
-# lat <- 44.4280
-# lon <- 110.5885
-# out <- ncdc(datasetid='GHCND', stationid='GHCND:USW00014895', datatypeid='PRCP', startdate = '2010-05-01', enddate = '2010-10-31', limit=500)
-# 
-# ##  Get forecasted snow
-# gefs_variables()
-# snow_depth = gefs("Snow_depth_surface_ens",lat,lon,raw=TRUE)
-
-# clim_dat <- read.csv("../data/idaho_climate.csv")
+##  Load snotel data, aggregate to get average snow depth by year
+snotel_raw <- read.csv("../data/west_yellowstone_snotel.csv", na.strings = "", stringsAsFactors = FALSE)
+snotel_raw <- snotel_raw[2:nrow(snotel_raw), ] # removes id row
+snotel_dat <- melt(snotel_raw, id.vars = c("Water.Year","Day"))
+snotel_dat$value <- as.numeric(snotel_dat$value)
+snotel_means <- ddply(snotel_dat, .(Water.Year), summarise,
+                      avg_snow = mean(value))
 
 ## Merge observation and climate data
-# bison_climate_dat <- merge(bison_dat, clim_dat)
+bison_climate_dat <- merge(bison_dat, snotel_means, by.x = "year", by.y="Water.Year")
+
+##  Retrieve climate data from NOAA for forecasts
+
+
 
 
 ####
 ####  Fit forecasting GLM
 ####
-my_model <- list(obs="count.mean", fixed=NULL, random=NULL, n.iter=5000)
-fitted_model <- fit_dlm_lnorm(model=my_model, data=bison_dat)
+my_model <- list(obs="count.mean", fixed=NULL, random=NULL, n.iter=10000)
+fitted_model <- fit_dlm_lnorm(model=my_model, data=bison_climate_dat)
 predictions <- rbind(fitted_model$predict[[1]],
                      fitted_model$predict[[2]],
                      fitted_model$predict[[3]])
@@ -68,7 +69,9 @@ prediction_df <- data.frame(year = bison_dat$year,
                             upper_prediction = exp(upper_predictions),
                             lower_prediction = exp(lower_predictions))
 
-
+# params <- as.data.frame(rbind(fitted_model$params[[1]],
+#                               fitted_model$params[[2]],
+#                               fitted_model$params[[3]]))
 
 ####
 ####  Plot the calibration data and predictions
@@ -83,5 +86,26 @@ ggplot(prediction_df, aes(x=year))+
   theme_few()
 ggsave(filename = "../figures/bison_calibration.png", width = 4, height = 3, units = "in", dpi=120)
 
+
+
+####
+####  Partition Forecast Uncertainty
+####
+nens <- 1
+nsteps <- 10
+meta.ds <- list()
+meta.ds[[1]] = 1:nens
+meta.ds[[2]] = max(bison_climate_dat$year):(max(bison_climate_dat$year)+nsteps)
+meta.ds[[3]] = c("Intercept")
+newdata = array(1,dim = c(nens,nsteps,1), dimnames = "Intercept")
+
+## just initial condition uncertainty
+FE_pred.I <- predict_dlm_lnorm(fit=fitted_model, newdata = newdata, n.iter=500,include="I", steps=nsteps, start.time = max(bison_climate_dat$year))
+
+## initial conditions + parameters
+FE_pred.IP <- predict_dlm_lnorm(fitted_model,n.iter=n.iter.fx,include=c("I","P"))
+
+plot_ss(meta.ds[[2]],FE_pred.I,ylab="NEE",xlab="time")
+plot_ss(meta.ds[[2]],FE_pred.IP,ylab="NEE",xlab="time")
 
 
